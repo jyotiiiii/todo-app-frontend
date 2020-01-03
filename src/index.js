3ee595609e0c6b60485a404c2a616fb2df2c020c
Jyoti <jyoti.contact@gmail.com>
2020-01-03 23:15:54 +0000

    install serialize-javascript

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+// ReactDOM.render(WHAT TO SHOW, WHERE TO SHOW IT, OPTIONAL CALLBACK TO TELL US WHEN THE FUNCTION WAS COMPLETED)
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
