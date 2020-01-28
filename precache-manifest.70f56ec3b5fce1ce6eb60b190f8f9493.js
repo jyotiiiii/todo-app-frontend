3a2ce5d693383834f82342613b2cd3c2453acc91
Jyoti <jyoti.contact@gmail.com>
2020-01-28 20:05:02 +0000

    Updates

---
+++ precache-manifest.70f56ec3b5fce1ce6eb60b190f8f9493.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc8bdafd38b5e6a94bb0e685b0a4883b",
+    "url": "/todo-app-frontend/index.html"
+  },
+  {
+    "revision": "69b9544a3d41257f481a",
+    "url": "/todo-app-frontend/static/js/2.b08dc323.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/todo-app-frontend/static/js/2.b08dc323.chunk.js.LICENSE"
+  },
+  {
+    "revision": "72f2aaa417a0627a1563",
+    "url": "/todo-app-frontend/static/js/main.7c4942a0.chunk.js"
+  },
+  {
+    "revision": "a61f3f6cae5543bf066c",
+    "url": "/todo-app-frontend/static/js/runtime-main.f4fe7c3c.js"
+  }
+]);
