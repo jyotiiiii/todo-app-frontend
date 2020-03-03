80968e4c4dd907c862ae982bcb8815b0853e139e
Jyoti <jyoti.contact@gmail.com>
2020-03-03 22:30:11 +0000

    update randomtasks.js

--- src/randomtasks.js
+++ src/randomtasks.js
@@ -4,6 +4,7 @@ const randomTasks = [
   {
     id: uuidv4(),
     user_id: '5cd7209f-b05b-456a-9327-55e8af3f945e',
+    random: 1,
     description: 'Sleep in',
     completed: 0,
     points: 5
@@ -11,6 +12,7 @@ const randomTasks = [
   {
     id: uuidv4(),
     user_id: '5cd7209f-b05b-456a-9327-55e8af3f945e',
+    random: 1,
     description: 'Go for a swim in the sea',
     completed: 0,
     points: 10
@@ -18,6 +20,7 @@ const randomTasks = [
   {
     id: uuidv4(),
     user_id: '5cd7209f-b05b-456a-9327-55e8af3f945e',
+    random: 1,
     description: 'Get a massage',
     completed: 0,
     points: 10
@@ -25,6 +28,7 @@ const randomTasks = [
   {
     id: uuidv4(),
     user_id: '5cd7209f-b05b-456a-9327-55e8af3f945e',
+    random: 1,
     description: 'Visit a new exhibition',
     completed: 0,
     points: 10
@@ -32,6 +36,7 @@ const randomTasks = [
   {
     id: uuidv4(),
     user_id: '5cd7209f-b05b-456a-9327-55e8af3f945e',
+    random: 1,
     description: 'Plan a holiday',
     completed: 0,
     points: 10
@@ -39,6 +44,7 @@ const randomTasks = [
   {
     id: uuidv4(),
     user_id: '5cd7209f-b05b-456a-9327-55e8af3f945e',
+    random: 1,
     description: 'Practice Mindfulness',
     completed: 0,
     points: 10
