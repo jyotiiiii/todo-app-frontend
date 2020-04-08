eaae63b4480a55a0b3cfe3503bf1644742ad3df8
Jyoti <jyoti.contact@gmail.com>
2020-04-08 21:09:05 +0100

    fix score to calculate completed tasks

--- src/components/App.js
+++ src/components/App.js
@@ -21,8 +21,11 @@ class App extends React.Component {
       .get('https://l9usbtfbs5.execute-api.eu-west-2.amazonaws.com/dev/tasks')
       .then(response => {
         // handle success
+        console.log(response.data.tasks);
+        console.log(this.calcScore(response.data.tasks));
         this.setState({
-          tasks: response.data.tasks
+          tasks: response.data.tasks,
+          score: this.calcScore(response.data.tasks)
         });
       })
       .catch(error => {
@@ -38,13 +41,13 @@ class App extends React.Component {
     let list = arr;
     // console.log({ listLine38: list });
     // initialise variable for total amount
-    let sum = this.state.score;
+    let sum = 0;
     // initialise empty array to collect points from each task object
     let scoresAdded = [];
     // loop through tasks array to pull out the points values
     for (let i = 0; i < list.length; i++) {
       // and put them in a scoresAdded array
-      if (list[i].completed === true) {
+      if (list[i].completed) {
         scoresAdded.push(list[i].points);
       }
     }
@@ -92,30 +95,35 @@ class App extends React.Component {
     // Firstly get the list of tasks from state
     const currentTasks = this.state.tasks;
     // Next, identify the task that matches the given task id
+    for (let i = 0; i < currentTasks.length; i++) {
+      const task = currentTasks[i];
+      // change the completed status of the matched task
+      if (task.id === taskId) {
+        // add a toggle to change status
+        task.completed = task.completed ? 0 : 1;
+        // task.completed = true if false, false if true;
+        // break;
+      }
+      // Update the state with the new completed status
+    }
 
+    console.log('beforeAxiosState ', this.state);
     axios
       .put(
-        `https://l9usbtfbs5.execute-api.eu-west-2.amazonaws.com/dev/tasks/${taskId}`
+        `https://l9usbtfbs5.execute-api.eu-west-2.amazonaws.com/dev/tasks/${taskId}`,
+        {
+          taskCompleted: currentTasks.find(task => task.id === taskId).completed
+        }
       )
       .then(response => {
         // handle success
-        for (let i = 0; i < currentTasks.length; i++) {
-          const task = currentTasks[i];
-          // change the completed status of the matched task
-          if (task.id === taskId) {
-            // add a toggle to change status
-            task.completed = task.completed ? false : true;
-            // task.completed = true if false, false if true;
-            // break;
-          }
-          // Update the state with the new completed status
-        }
         this.setState({
           tasks: currentTasks
         });
+        console.log(response);
       })
       .catch(error => {
-        // handle error
+        // handle error to alert user
         console.error(error);
       });
   };
